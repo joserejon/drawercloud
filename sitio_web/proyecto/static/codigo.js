e4ea88e7f7881b7a8f2ea0af4309b2adcfc97545
joserejon <jose.rejon.santiago@gmail.com>
2017-08-01 11:04:26 +0200

    botón derecho

--- sitio_web/proyecto/static/codigo.js
+++ sitio_web/proyecto/static/codigo.js
@@ -4,46 +4,6 @@ $( document ).ready(function() {
         $('.row-offcanvas').toggleClass('active')
     });
     
-    /******************* OPCIONES DE BOTÓN DERECHO EN LA LISTA *******************/
-    //Ocultamos el menú al cargar la página
-    $("#menu").hide();
-    
-    //El código referente a mostrar el menú emergente se encuentra en cada modo de vista
-
-    //Cuando hagamos click, el menú desaparecerá
-    $(document).click(function(e){
-        if(e.button == 0){
-            $("#menu").css("display", "none");
-        }
-    });
-    //Si pulsamos esc, el menú desaparecerá
-    $(document).keydown(function(e){
-        if(e.keyCode == 27){
-            $("#menu").css("display", "none");
-        }
-    });
-    //Acciones de los botones del menú
-    $("#menu").click(function(e){
-        //El switch utiliza los IDs de los <li> del menú
-        switch(e.target.id){
-            case "descargar":
-                alert("Descargando");
-                break;
-            case "copiar":
-                alert("Copiado");
-                break;
-            case "mover":
-                alert("Movido");
-                break;
-            case "addfavoritos":
-                alert("Añadido a favoritos");
-                break;
-            case "eliminar":
-                alert("Eliminado");
-                break;
-        }
-    });
-
 
     /******************* MOVIMIENTO MENÚ DERECHA *******************/
     var altura = $('#bt_offcanvas').offset().top;
