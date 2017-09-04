7ed297c835d395044094315eb794995fd973a94f
joserejon <jose.rejon.santiago@gmail.com>
2017-09-04 17:52:12 +0200

    Registro un paso v3

--- sitio_web/sitio_web/urls.py
+++ sitio_web/sitio_web/urls.py
@@ -24,5 +24,6 @@ class MyRegistrationView(RegistrationView):
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^proyecto/', include('proyecto.urls')),
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
     url(r'^accounts/', include('registration.backends.simple.urls')),
 ]
